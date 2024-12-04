f3b9376249cfd90a297807e635e3f5598c868007
ums_L200220212 <120449086+ums-L200220212@users.noreply.github.com>
2024-12-04 22:15:33 +0700

    Delete forum-review-film directory

--- forum-review-film/resources/views/pages/about.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('mainlayout')
-
-@section('maincontent')
-<div class="container" style="text-align: center">
-    <label>ini halaman about</label></br>
-    <label>Forum Review Film Adalah Forum untuk para pecinta film yang ingin berdiskusi dengan pecinta film lainnya</label>
-</div>
-    
-@endsection
